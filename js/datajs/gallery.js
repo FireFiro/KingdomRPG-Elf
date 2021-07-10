5e7f321798aff294004b1a1ba0ce077f1ee9fade
_FireFrio_ <43435417+FireFiro@users.noreply.github.com>
2021-07-10 21:52:32 +1000

    Add Page Role

---
+++ js/datajs/gallery.js
@@ -0,0 +1,13 @@
+var gallery = [
+    {
+        "name": "Поиск Сокровищ",
+        "image": "./img/Event/Поиск_сокровищ.png",
+        "date": "03.07.2021"
+    },
+    {
+        "name": "Fera-im-as",
+        "image": "./img/Event/Fera-im-as.png",
+        "date": "04.07.2021"
+    }
+
+]
---
+++ js/websuitejs/role.js
@@ -0,0 +1,18 @@
+createTable()
+function createTable() {
+    role.forEach(e => {
+        addTable(e)
+    })
+}
+
+function addTable(role) {
+   let div = document.querySelector("#body")
+   let HTML = `
+    <tr>
+        <th class="center">${role.category}</th>
+        <td class="center"><img src="${role.image}"><br>${role.name}</td>
+        <td class="center">${role.description}</td>
+        <td>${role.mechanics}</td>
+    </tr>`
+    div.insertAdjacentHTML(`beforeend`, HTML)
+}
